e2a550a26d9bbdd00c4a9ed892e58e654f230a1b
SangMin <sangmin4208@gmail.com>
2023-06-10 15:27:31 +0900

    ✍️ 9.5 페이지 레이아웃 만들기

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,20 +1,18 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function Header() {
   return (
-    <header>
-        <Link
-            href="/"
-        >
-            <h1>{`Sangmin's Blog`}</h1>
-        </Link>
-        <nav>
-            <Link href={'/'}>home</Link>
-            <Link href={'/about'}>about</Link>
-            <Link href={'/posts'}>posts</Link>
-            <Link href={'/contact'}>contact</Link>
-        </nav>
+    <header className="w-full flex justify-between items-center py-4">
+      <Link href="/">
+        <h1 className="text-4xl">{`Sangmin's Blog`}</h1>
+      </Link>
+      <nav className="flex gap-4 uppercase text-lg">
+        <Link href={"/"}>home</Link>
+        <Link href={"/about"}>about</Link>
+        <Link href={"/posts"}>posts</Link>
+        <Link href={"/contact"}>contact</Link>
+      </nav>
     </header>
-  )
+  );
 }
