9432ad03128202069f0e31509e8b779545d3440b
SangMin <sangmin4208@gmail.com>
2023-06-10 16:43:29 +0900

    🎨 header style 추가

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,18 +1,40 @@
-import Link from "next/link";
-import React from "react";
-
+"use client"
+import Link from "next/link"
+import React from "react"
+import { useSelectedLayoutSegment } from "next/navigation"
 export default function Header() {
+  const selectedLayoutSegment = useSelectedLayoutSegment()
   return (
     <header className="w-full flex justify-between items-center py-4">
       <Link href="/">
-        <h1 className="text-4xl">{`Sangmin's Blog`}</h1>
+        <h1 className="font-bold text-4xl">{`SangMin's Blog`}</h1>
       </Link>
-      <nav className="flex gap-4 uppercase text-lg">
-        <Link href={"/"}>home</Link>
-        <Link href={"/about"}>about</Link>
-        <Link href={"/posts"}>posts</Link>
-        <Link href={"/contact"}>contact</Link>
+      <nav className="flex gap-4 uppercase">
+        <Link
+          href={"/"}
+          className={selectedLayoutSegment === null ? "font-bold" : ""}
+        >
+          home
+        </Link>
+        <Link
+          className={selectedLayoutSegment === "about" ? "font-bold" : ""}
+          href={"/about"}
+        >
+          about
+        </Link>
+        <Link
+          className={selectedLayoutSegment === "posts" ? "font-bold" : ""}
+          href={"/posts"}
+        >
+          posts
+        </Link>
+        <Link
+          className={selectedLayoutSegment === "contact" ? "font-bold" : ""}
+          href={"/contact"}
+        >
+          contact
+        </Link>
       </nav>
     </header>
-  );
+  )
 }
