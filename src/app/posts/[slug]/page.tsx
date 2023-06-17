cf663bd5ea3140cf7c9a109a7390bd444e9c22d3
SangMin <sangmin4208@gmail.com>
2023-06-17 19:44:45 +0900

    🎨 10.3 마크다운 뷰어 상세정보

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,9 @@
 import MarkdownViewer from "@/app/components/markdown-viewer"
 import { getPostBySlug } from "@/services/posts"
+import Image from "next/image"
 import { notFound } from "next/navigation"
 import React from "react"
-
+import { AiTwotoneCalendar } from "react-icons/ai"
 interface Params {
   params: {
     slug: string
@@ -11,15 +12,29 @@ interface Params {
 
 export default async function Page({ params: { slug } }: Params) {
   const post = await getPostBySlug(slug)
+  const { path, content, title, description } = post
   if (!post) {
     notFound()
   }
   return (
-    <div>
-      <h1>
-        {post.title} ({post.path})
-      </h1>
-      <MarkdownViewer>{post.content}</MarkdownViewer>
-    </div>
+    <article className="m-4 ">
+      <Image
+        className="object-cover w-full max-h-1/5 rounded-2xl"
+        src={`/images/posts/${path}.png`}
+        width={760}
+        height={400}
+        alt={title}
+      />
+      <section className="flex flex-col gap-2 p-4">
+        <div className="flex items-center gap-2">
+          <AiTwotoneCalendar size={24} />
+          <h1 className="text-3xl">{title}</h1>
+        </div>
+        <p className="text-sm font-bold">{description}</p>
+      </section>
+      <main className="mt-4">
+        <MarkdownViewer>{content}</MarkdownViewer>
+      </main>
+    </article>
   )
 }
