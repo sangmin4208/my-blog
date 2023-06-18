d3ccc86bdbb452119c52f5c319942a91c3797600
SangMin <sangmin4208@gmail.com>
2023-06-18 12:06:49 +0900

    🎨10.4 마크다운 뷰어 - 코드 문법

--- src/app/components/markdown-viewer.tsx
+++ src/app/components/markdown-viewer.tsx
@@ -1,7 +1,9 @@
+"use client"
 import React from "react"
 import { ReactMarkdown } from "react-markdown/lib/react-markdown"
+import SyntaxHighlighter from "react-syntax-highlighter"
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/hljs"
 import remarkGfm from "remark-gfm"
-
 interface Props {
   children: string
 }
@@ -10,6 +12,25 @@ export default function MarkdownViewer({ children }: Props) {
     <ReactMarkdown
       className="prose dark:prose-invert lg:prose-xl"
       remarkPlugins={[remarkGfm]}
+      components={{
+        code({ node, inline, className, children, style, ...props }) {
+          const match = /language-(\w+)/.exec(className || "")
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              style={dracula}
+              language={match[1]}
+              PreTag="div"
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          )
+        },
+      }}
     >
       {children}
     </ReactMarkdown>
